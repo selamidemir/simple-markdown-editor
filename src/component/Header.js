4f5d21717ea49d8d2cc8b6eb726c8782fb8fa20d
Selami Demir <selamidemir@gmail.com>
2022-08-10 12:27:52 +0300

    Added some styles and new favicon, changed title and description

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='header'><h1>Markdown Preview</h1></div>
+    <div className='header'><h1>Markdown Editor</h1></div>
   )
 }
 

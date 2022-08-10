dd3c5874726fa458cfb70e805dd89f3d1f4fdf0b
Selami Demir <selamidemir@gmail.com>
2022-08-10 12:09:20 +0300

    Added some styles and react-from-dom module to convert string to html

--- src/component/Preview.js
+++ src/component/Preview.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectHtmlContent } from '../redux/editorSlice';
+import convert from 'react-from-dom';
 
 function Preview() {
     const content = useSelector(selectHtmlContent);
 
     return (
-        <div>
-            <pre>{content}</pre>
+        <div className='preview'>
+            {convert(`<div>${content}</div>`)}
         </div>
     )
 }

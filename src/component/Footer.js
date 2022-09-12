e133ed5ccc1ef3fd315ba7f7b8f461425d0fb239
Selami <selamidemir@gmail.com>
2022-09-12 22:47:08 +0300

    Footer düzenlendi

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className='footer'>Designed and Coded By Selami Demir</div>
+    <div className='footer'>Designed and Coded By Selami Demir | 
+    <a href="https://github.com/selamidemir" title="github selami demir">Gihub</a> | 
+    <a href="https://netbilio.com" title='Netbilio Teknoloji'>Netbilio</a>
+    </div>
   )
 }
 
